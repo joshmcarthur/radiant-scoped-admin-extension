fa5fce90cfca811f97874754201acc513fb19626
Josh McArthur <joshua.mcarthur@gmail.com>
2011-02-01 23:54:10 +1300

    Allow admin user automatic access to roles

--- lib/scoped_admin/admin_roles_controller_ext.rb
+++ lib/scoped_admin/admin_roles_controller_ext.rb
@@ -4,7 +4,7 @@ module ScopedAdmin
     def self.included(base)
       base.class_eval do        
         only_allow_access_to :index, :show, :new, :create, :edit, :update, :remove, :destroy,
-          :when => [:coordinator],
+          :when => [:coordinator, :admin],
           :denied_url => { :controller => 'pages', :action => 'index' },
           :denied_message => 'You must have the correct privileges to perform this action.'       
       end
